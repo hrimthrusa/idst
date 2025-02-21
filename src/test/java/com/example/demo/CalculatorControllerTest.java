fe4a6f9e9a5279b1ec0f67045826d2938c3b84c6
xturn <xturnerrx@gmail.com>
2025-02-21 15:27:29 +0100

    Multiply added

--- src/test/java/com/example/demo/CalculatorControllerTest.java
+++ src/test/java/com/example/demo/CalculatorControllerTest.java
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(CalculatorController.class)
 public class CalculatorControllerTest {
 
+
 //    @Autowired
 //    private MockMvc mockMvc;
 //
